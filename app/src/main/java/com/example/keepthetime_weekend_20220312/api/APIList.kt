29674a0a1620d4cff13a3bc320bf2bb2fa3afeeb
이형종 <hj36879@naver.com>
2022-03-12 14:33:03 +0900

    formData에 데이터 첨부 : @FormUrlEncoded 문구 추가

--- app/src/main/java/com/example/keepthetime_weekend_20220312/api/APIList.kt
+++ app/src/main/java/com/example/keepthetime_weekend_20220312/api/APIList.kt
@@ -4,6 +4,7 @@ package com.example.keepthetime_weekend_20220312.api
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 // keepthetime.xyz 서버에 있는 기능에 접속하는 방법을 명시하는 인터페이스.
@@ -11,8 +12,8 @@ import retrofit2.http.POST
 interface APIList {
 
 //    로그인 기능 : POST - /user
-
-    @POST("/user")
+   @FormUrlEncoded // POST / PUT / PATCH - formData(앱코드 : Field) 에 데이터 첨부시에 필요한 코드드
+   @POST("/user")
     fun postRequestLogin(
         @Field("email") id: String,
         @Field("password") pw : String,
