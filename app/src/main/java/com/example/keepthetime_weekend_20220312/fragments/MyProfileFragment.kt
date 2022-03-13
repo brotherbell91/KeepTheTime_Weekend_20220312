9fd66bc13c062f34ca8362f4cfa142b78b317c60
이형종 <hj36879@naver.com>
2022-03-13 15:54:37 +0900

    내 정보 프래그먼트 - BaseFragment 상속받아 완성

---
+++ app/src/main/java/com/example/keepthetime_weekend_20220312/fragments/MyProfileFragment.kt
@@ -0,0 +1,35 @@
+package com.example.keepthetime_weekend_20220312.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.keepthetime_weekend_20220312.R
+
+class MyProfileFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+
+    }
+
+
+
+
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
