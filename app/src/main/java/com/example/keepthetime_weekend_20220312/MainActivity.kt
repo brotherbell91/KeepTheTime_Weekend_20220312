1a5a1f9649b1a53bb8b11e4e8b7c464b299fcd86
이형종 <hj36879@naver.com>
2022-03-13 16:02:43 +0900

    메인화면 뷰페이저 어댑터 적용

--- app/src/main/java/com/example/keepthetime_weekend_20220312/MainActivity.kt
+++ app/src/main/java/com/example/keepthetime_weekend_20220312/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.keepthetime_weekend_20220312
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.example.keepthetime_weekend_20220312.adapters.MainViewPagerAdapter
 import com.example.keepthetime_weekend_20220312.databinding.ActivityMainBinding
 import com.example.keepthetime_weekend_20220312.datas.BasicResponse
 import com.example.keepthetime_weekend_20220312.utils.ContextUtil
@@ -13,6 +14,7 @@ import retrofit2.Response
 class MainActivity : BaseActivity() {
 
     lateinit var binding : ActivityMainBinding
+    lateinit var mAdapter : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +28,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mAdapter = MainViewPagerAdapter(supportFragmentManager)
+        binding.mainViewPager.adapter = mAdapter
 
 
     }
