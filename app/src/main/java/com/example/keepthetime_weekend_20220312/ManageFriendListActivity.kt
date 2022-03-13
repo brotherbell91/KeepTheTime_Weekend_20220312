7db2f3d0d1212755350705255e20ec4008152647
이형종 <hj36879@naver.com>
2022-03-13 18:38:37 +0900

    실제 친구 목록을 채울 ArrayList 생성

--- app/src/main/java/com/example/keepthetime_weekend_20220312/ManageFriendListActivity.kt
+++ app/src/main/java/com/example/keepthetime_weekend_20220312/ManageFriendListActivity.kt
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_weekend_20220312.databinding.ActivityManageFriendListBinding
+import com.example.keepthetime_weekend_20220312.datas.UserData
 
 class ManageFriendListActivity : BaseActivity() {
 
     lateinit var binding : ActivityManageFriendListBinding
+
+//    내 친구 목록을 담아줄 그릇
+    val mMyFriendList = ArrayList<UserData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_friend_list)
@@ -20,5 +25,10 @@ class ManageFriendListActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        내 친구 목록에 데이터 채우기.
+//         => 서버가 실제로 내려주는 친구목록을 채워보자. (API 통신과 결합)
+
+
     }
 }
