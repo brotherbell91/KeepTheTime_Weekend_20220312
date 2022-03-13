02ad9bdedc5d1b68e600f456e6216a61801691fc
이형종 <hj36879@naver.com>
2022-03-13 18:12:39 +0900

    친구 목록 관리 화면 추가

---
+++ app/src/main/java/com/example/keepthetime_weekend_20220312/ManageFriendListActivity.kt
@@ -0,0 +1,24 @@
+package com.example.keepthetime_weekend_20220312
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.keepthetime_weekend_20220312.databinding.ActivityManageFriendListBinding
+
+class ManageFriendListActivity : BaseActivity() {
+
+    lateinit var binding : ActivityManageFriendListBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_friend_list)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
