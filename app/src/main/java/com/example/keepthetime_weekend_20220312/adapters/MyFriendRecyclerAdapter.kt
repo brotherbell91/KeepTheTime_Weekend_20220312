01f829a7631d7676a97f734df0cf99738d4c3f2f
이형종 <hj36879@naver.com>
2022-03-19 11:46:16 +0900

    리사이클러뷰의 어댑터 작업중  - 3개의 필수 구현 함수 오버라이딩  - 필요 자료 2개를 멤버변수로 저장. (생성자 활용)

--- app/src/main/java/com/example/keepthetime_weekend_20220312/adapters/MyFriendRecyclerAdapter.kt
+++ app/src/main/java/com/example/keepthetime_weekend_20220312/adapters/MyFriendRecyclerAdapter.kt
@@ -1,13 +1,31 @@
 package com.example.keepthetime_weekend_20220312.adapters
 
+import android.content.Context
+import android.service.autofill.UserData
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class MyFriendRecyclerAdapter : RecyclerView.Adapter<>() {
+class MyFriendRecyclerAdapter(
+    val mContext : Context,
+    val mList : List<UserData> //뷰 홀더 사용하면, resID를 받지 않아도 됨.
+) : RecyclerView.Adapter<MyFriendRecyclerAdapter.MyViewHolder>() {
 
 //    클래스 내부의 클래스(inner class) 제작 > MyFriendRecyclerAdapter가 혼자 사용.
 
     inner class MyViewHolder(view : View) : RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
 
     }
 
