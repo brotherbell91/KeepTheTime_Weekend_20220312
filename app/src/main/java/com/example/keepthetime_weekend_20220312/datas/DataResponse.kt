63159c73e04b9ac1c21665b9c0b57ca7e403b3f5
이형종 <hj36879@naver.com>
2022-03-19 10:47:02 +0900

    서버가 주는 친구 목록 데이터 파싱

--- app/src/main/java/com/example/keepthetime_weekend_20220312/datas/DataResponse.kt
+++ app/src/main/java/com/example/keepthetime_weekend_20220312/datas/DataResponse.kt
@@ -2,9 +2,9 @@ package com.example.keepthetime_weekend_20220312.datas
 
 class DataResponse(
 //    val user : ?? 현재는 skip
-    val user : UserData,
+    val user : UserData, //로그인 / 회원가입 API가 호출되면 사용할 데이터.
     val token : String,
 
-
+    val friends : List< UserData >, // 친구목록 API가 호출되면 사용할 데이터.
 ) {
 }
