27f0c7d945fa165c5f86d5aedc1ecf7f7babd38f
이형종 <hj36879@naver.com>
2022-03-19 12:18:40 +0900

    리싸이클러뷰의 어댑터 생성 > 리싸이클러뷰에 연결  - 누락된 데이터바인딩 세팅 진행  - 아직 화면에 목록이 나오지 않는다.

--- app/src/main/java/com/example/keepthetime_weekend_20220312/fragments/MyFriendListFragment.kt
+++ app/src/main/java/com/example/keepthetime_weekend_20220312/fragments/MyFriendListFragment.kt
@@ -5,8 +5,11 @@ import android.service.autofill.UserData
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+
 import com.example.keepthetime_weekend_20220312.R
+import com.example.keepthetime_weekend_20220312.adapters.MyFriendRecyclerAdapter
+import com.example.keepthetime_weekend_20220312.databinding.FragmentMyFriendListBinding
 import com.example.keepthetime_weekend_20220312.datas.BasicResponse
 import retrofit2.Call
 import retrofit2.Callback
@@ -15,30 +18,39 @@ import retrofit2.Response
 
 class MyFriendListFragment : BaseFragment() {
 
+    lateinit var binding: FragmentMyFriendListBinding
+
     val mMyFriendList = ArrayList<UserData>()
+
+    lateinit var mAdapter: MyFriendRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_friend_list,container,false)
+        binding =  DataBindingUtil.inflate(inflater, R.layout.fragment_my_friend_list, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         setupEvents()
         setValues()
-
     }
 
 
     override fun setupEvents() {
+
     }
 
     override fun setValues() {
 
         getMyFriendListFromServer()
+
+        mAdapter = MyFriendRecyclerAdapter(mContext, mMyFriendList)
+        binding.myFriendRecyclerView.adapter = mAdapter
+
     }
 
     fun getMyFriendListFromServer() {
@@ -63,5 +75,8 @@ class MyFriendListFragment : BaseFragment() {
             }
 
         })
+
+    }
+
 
 }
