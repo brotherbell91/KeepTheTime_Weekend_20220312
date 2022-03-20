aff4d98f50f1ab0e51a9771c9145b50f21af8896
이형종 <hj36879@naver.com>
2022-03-20 11:41:19 +0900

    SimpleDateFormat 활용, 선택한 일자를 지정된 양식으로 표현.

--- app/src/main/java/com/example/keepthetime_weekend_20220312/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_weekend_20220312/EditAppointmentActivity.kt
@@ -3,10 +3,12 @@ package com.example.keepthetime_weekend_20220312
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.DatePicker
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_weekend_20220312.databinding.ActivityEditAppointmentBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
 class EditAppointmentActivity : BaseActivity() {
@@ -15,7 +17,7 @@ class EditAppointmentActivity : BaseActivity() {
     
 //   선택하는 약속일시를 저장하는 calendar 변수
 //    calendar는 java로 선택
-    val mSelectedDateTimeCal = Calendar.getInstance()
+    val mSelectedDateTimeCal = Calendar.getInstance() //현재 일시가 저장됨 (일시 + 초 + 1/1000초)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,19 +42,32 @@ class EditAppointmentActivity : BaseActivity() {
 //                    선택된 일시를 저장할 변수에, 연/월/일 세팅.
                     mSelectedDateTimeCal.set(year, month, dayOfMonth)
 
+//                    약속 일자 텍스트뷰의 문구를 "3월 20일" 형태로 가공해서 출력.
+//                    calendar(내부의 Date) 를 => String 으로 가공 전문 클래스(SimpleDateFormat) 활용.
+                    
+                    val sdf = SimpleDateFormat("M월 d일")
+                    
+//                    sdf로 format해낸 String을, txtDate의 문구로 반영
+//                    format은 date로 선택
+                    binding.txtDate.text = sdf.format(mSelectedDateTimeCal.time)
+
                 }
 
             }
 
 //            실제로 달력 팝업 띄우기.
 
+//            선택한 일시 ( 기본값 : 현재일시) 의 연/월/일을 띄워보자.
+
             val dpd = DatePickerDialog(
                 mContext,
                 dsl,
-                2022,
-                3,
-                20
+                mSelectedDateTimeCal.get(Calendar.YEAR), //선택일시의 년도만 배치.
+                mSelectedDateTimeCal.get(Calendar.MONTH),
+                mSelectedDateTimeCal.get(Calendar.DAY_OF_MONTH)
             ).show()
+
+            Log.d("선택월", mSelectedDateTimeCal.get(Calendar.MONTH).toString())
 
         }
 
