d86592383edcea04728daf288ad59041631feb2e
이형종 <hj36879@naver.com>
2022-03-26 13:58:56 +0900

    JSONObject 형태의 응답 파싱 예시  - {  } : resultObj  - 8542 등 Int :  pointDistance

--- app/src/main/java/com/example/keepthetime_weekend_20220312/ViewMapActivity.kt
+++ app/src/main/java/com/example/keepthetime_weekend_20220312/ViewMapActivity.kt
@@ -2,6 +2,7 @@ package com.example.keepthetime_weekend_20220312
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_weekend_20220312.databinding.ActivityViewMapBinding
 import com.example.keepthetime_weekend_20220312.datas.AppointmentData
@@ -80,6 +81,16 @@ class ViewMapActivity : BaseActivity() {
                 object : OnResultCallbackListener {
                     override fun onSuccess(p0: ODsayData?, p1: API?) {
                         // 길찾기 응답이 돌아오면 할 일.
+
+                        val jsonObj = p0!!.json  // 길찾기 응답이 돌아온 JSONObject를 변수에 저장.
+                        Log.d("길찾기응답", jsonObj.toString())
+
+//                        jsonObj의 내부에서, => result라는 이름표를 가진 {  } 추출
+//                        result가 JSONObject라고 명시 : resultObj로 변수 이름 설정.
+                        val resultObj =  jsonObj.getJSONObject("result")
+
+//                        result: { } 안에서, pointDistance 라는 이름표의 Int값 추출 예시.
+                        val pointDistance = resultObj.getInt("pointDistance")
 
 
                     }
